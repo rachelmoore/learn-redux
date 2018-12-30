9ef6580c4c52565b3bd31a5e43fccc1f646606d8
Rachel Moore <rachel@rachelmoore.net>
2018-12-29 23:31:49 -0800

    add double comments instead of deleting them lol

--- client/components/Single.js
+++ client/components/Single.js
@@ -8,10 +8,11 @@ const Single = React.createClass({
         const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
         //get us the post
         const post = this.props.posts[i];
+        const postComments = this.props.comments[this.props.params.postId] || [];
         return (
             <div className="single-photo">
                 <Photo i={i} post={post} {...this.props} />
-                <Comments />
+                <Comments postComments={postComments} {...this.props}/>
             </div>
         )
     }
